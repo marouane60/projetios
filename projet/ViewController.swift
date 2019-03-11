6b36d3620f80eb740b9b6c77b4cb272f54f54ef7
tp <tp@tps-iMac.local>
2019-03-11 18:29:20 +0100

    first view logic done

--- projet/ViewController.swift
+++ projet/ViewController.swift
@@ -14,41 +14,63 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var maxTemp: UITextView!
+    @IBOutlet weak var favoriteButton: UIButton!
     var locationManager:CLLocationManager!
-
+    var favorite: Bool! = false;
+    var villeParam: Bool! = false;
+    var villeRequete: String!;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        Alamofire.request("https://api.openweathermap.org/data/2.5/weather?id=2989317&APPID=0fe58b4b34de9e8260b69024f643a82a").validate().responseJSON { response in
-            switch response.result {
-            case .success:
-                let json = JSON(response.result.value!)
-                
-        print(json["coord"]["lat"])
-            case .failure(let error):
-                print(error)
-            }
-        }
-        
-        // https://stackoverflow.com/questions/25296691/get-users-current-location-coordinates
         
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
+    
         
-        if CLLocationManager.locationServicesEnabled(){
-            locationManager.startUpdatingLocation()
+        if(villeParam){
+            
+            // villeRequete = navParams
+            
+        }else{
+            let favorite =  UserDefaults.standard.string(forKey: "favorite")
+            
+            if(favorite != nil){
+                
+                villeRequete = favorite;
+                
+            }else{
+                if CLLocationManager.locationServicesEnabled(){
+                    locationManager.startUpdatingLocation()
+                }
+            }
+            
         }
-
         
+        //Get ville infos
+        //TODO: add asynchrone handling
+        Alamofire.request("https://api.openweathermap.org/data/2.5/weather?q=" + villeRequete + "&APPID=0fe58b4b34de9e8260b69024f643a82a").validate().responseJSON { response in
+            switch response.result {
+            case .success:
+                let json = JSON(response.result.value!)
+                
+                print(json["name"])
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+        
+        // Onclick on the favorite button
         
     
 
         
     }
     
+    // https://stackoverflow.com/questions/25296691/get-users-current-location-coordinates
     //MARK: - location delegate methods
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let userLocation :CLLocation = locations[0] as CLLocation
@@ -63,15 +85,20 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             }
             let placemark = placemarks! as [CLPlacemark]
             if placemark.count>0{
-                let placemark = placemarks![0]
-                print(placemark.locality!)
-                print(placemark.administrativeArea!)
-                print(placemark.country!)
+                let placemark = placemarks![0];
+                let locality = placemark.locality!;
+                let localityFormatted = locality.replacingOccurrences(of: " ", with: "%20").replacingOccurrences(of: "é", with: "e")
+                self.villeRequete = localityFormatted;
                 
             }
         }
         
     }
+    
+    @IBAction func onFavorite(_ sender: Any) {
+        UserDefaults.standard.set(villeRequete, forKey: "favorite");
+    }
+    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Error \(error)")
     }
