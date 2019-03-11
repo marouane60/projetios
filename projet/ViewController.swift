41b0ec3056268f30619f4eeb74a90480ce9518e1
tp <tp@tps-iMac.local>
2019-03-11 17:00:07 +0100

    retrieve city name

--- projet/ViewController.swift
+++ projet/ViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var maxTemp: UITextView!
-    
+    var locationManager:CLLocationManager!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,16 +25,56 @@ class ViewController: UIViewController {
             case .success:
                 let json = JSON(response.result.value!)
                 
-        print(json["name"])
+        print(json["coord"]["lat"])
             case .failure(let error):
                 print(error)
             }
         }
         
+        // https://stackoverflow.com/questions/25296691/get-users-current-location-coordinates
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.startUpdatingLocation()
+        }
+
+        
+        
+    
 
         
     }
-
+    
+    //MARK: - location delegate methods
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation :CLLocation = locations[0] as CLLocation
+        
+        print("user latitude = \(userLocation.coordinate.latitude)")
+        print("user longitude = \(userLocation.coordinate.longitude)")
+        
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(userLocation) { (placemarks, error) in
+            if (error != nil){
+                print("error in reverseGeocode")
+            }
+            let placemark = placemarks! as [CLPlacemark]
+            if placemark.count>0{
+                let placemark = placemarks![0]
+                print(placemark.locality!)
+                print(placemark.administrativeArea!)
+                print(placemark.country!)
+                
+            }
+        }
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error \(error)")
+    }
 
 }
 
