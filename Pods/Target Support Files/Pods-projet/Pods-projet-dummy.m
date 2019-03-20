f4f0c5e35849beb7e90656a1152094b178b2632f
tp <tp@tps-iMac.local>
2019-03-20 13:23:56 +0100

    menage

--- Pods/Target Support Files/Pods-projet/Pods-projet-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_projet : NSObject
-@end
-@implementation PodsDummy_Pods_projet
-@end
--- Pods/Target Support Files/Pods-projetTests/Pods-projetTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_projetTests : NSObject
-@end
-@implementation PodsDummy_Pods_projetTests
-@end
